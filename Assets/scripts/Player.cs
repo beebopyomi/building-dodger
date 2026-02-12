2f08be6d76678684d6ff558b22b96db25c3551ac
shikanokonokonokokoshitantan <jakubszymanzki@gmail.com>
2026-02-12 14:38:28 +0100

    hi
    hi hi

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,13 +2,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float movespeed = 5f;
+    public float jumpforce = 10f;
+    public Transform groundcheck;
+    public float groundcheckradius = 0.2f;
+    public LayerMask GroundLayer;
+    private Rigidbody2D rb;
+    private bool isgrounded;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        float moveinput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveinput * movespeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isgrounded = Physics2D.OverlapCircle(groundcheck.position, groundcheckradius, GroundLayer);
     }
 }

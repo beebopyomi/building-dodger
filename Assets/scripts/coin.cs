f8bbe316b1277dbd81d8607bdc6f89938377a78c
shikanokonokonokokoshitantan <jakubszymanzki@gmail.com>
2026-02-20 22:32:59 +0100

    hi
    hi

--- Assets/scripts/coin.cs
+++ Assets/scripts/coin.cs
@@ -1,14 +1,22 @@
 using System.Security.Cryptography;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class coin : MonoBehaviour
 {
+    private TextMeshProUGUI coinText;
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Player  player = collision.gameObject.GetComponent<Player>();
             player.coins += 1;
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
